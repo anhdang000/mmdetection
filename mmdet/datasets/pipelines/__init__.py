0c66400145d5f636631ea6f3de9a71b5196c2ec0
anhdang000 <danglinh.anh000@gmail.com>
2021-07-05 14:19:01 +0700

    Add data pipline classes

--- mmdet/datasets/pipelines/__init__.py
+++ mmdet/datasets/pipelines/__init__.py
@@ -12,15 +12,21 @@ from .transforms import (Albu, CutOut, Expand, MinIoURandomCrop, Normalize,
                          Pad, PhotoMetricDistortion, RandomCenterCropPad,
                          RandomCrop, RandomFlip, RandomShift, Resize,
                          SegRescale)
+from .loadingLP import LoadImageFromFileLP, LoadAnnotationsLP                         
+from .transformsLP import (ResizeLP, RandomFlipLP, NormalizeLP, PadLP)     
+from .formatingLP import ImageToTensorLP, DefaultFormatBundleLP, CollectLP
+from .test_time_augLP import MultiScaleFlipAugLP            
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
-    'Transpose', 'Collect', 'DefaultFormatBundle', 'LoadAnnotations',
+    'Transpose', 'Collect', 'DefaultFormatBundle', 'LoadAnnotations', 
     'LoadImageFromFile', 'LoadImageFromWebcam',
     'LoadMultiChannelImageFromFiles', 'LoadProposals', 'MultiScaleFlipAug',
     'Resize', 'RandomFlip', 'Pad', 'RandomCrop', 'Normalize', 'SegRescale',
     'MinIoURandomCrop', 'Expand', 'PhotoMetricDistortion', 'Albu',
     'InstaBoost', 'RandomCenterCropPad', 'AutoAugment', 'CutOut', 'Shear',
     'Rotate', 'ColorTransform', 'EqualizeTransform', 'BrightnessTransform',
-    'ContrastTransform', 'Translate', 'RandomShift'
+    'ContrastTransform', 'Translate', 'RandomShift', 'LoadImageFromFileLP',
+    'LoadAnnotationsLP', 'ResizeLP', 'RandomFlipLP', 'NormalizeLP', 'PadLP',
+    'ImageToTensorLP', 'DefaultFormatBundleLP', 'CollectLP', 'MultiScaleFlipAugLP'
 ]
--- mmdet/datasets/pipelines/formatingLP.py
+++ mmdet/datasets/pipelines/formatingLP.py
@@ -63,7 +63,7 @@ class ToTensor:
 
 
 @PIPELINES.register_module()
-class ImageToTensor:
+class ImageToTensorLP:
     """Convert image to :obj:`torch.Tensor` by given keys.
 
     The dimension order of input image is (H, W, C). The pipeline will convert
@@ -100,78 +100,7 @@ class ImageToTensor:
 
 
 @PIPELINES.register_module()
-class Transpose:
-    """Transpose some results by given keys.
-
-    Args:
-        keys (Sequence[str]): Keys of results to be transposed.
-        order (Sequence[int]): Order of transpose.
-    """
-
-    def __init__(self, keys, order):
-        self.keys = keys
-        self.order = order
-
-    def __call__(self, results):
-        """Call function to transpose the channel order of data in results.
-
-        Args:
-            results (dict): Result dict contains the data to transpose.
-
-        Returns:
-            dict: The result dict contains the data transposed to \
-                ``self.order``.
-        """
-        for key in self.keys:
-            results[key] = results[key].transpose(self.order)
-        return results
-
-    def __repr__(self):
-        return self.__class__.__name__ + \
-            f'(keys={self.keys}, order={self.order})'
-
-
-@PIPELINES.register_module()
-class ToDataContainer:
-    """Convert results to :obj:`mmcv.DataContainer` by given fields.
-
-    Args:
-        fields (Sequence[dict]): Each field is a dict like
-            ``dict(key='xxx', **kwargs)``. The ``key`` in result will
-            be converted to :obj:`mmcv.DataContainer` with ``**kwargs``.
-            Default: ``(dict(key='img', stack=True), dict(key='gt_bboxes'),
-            dict(key='gt_labels'))``.
-    """
-
-    def __init__(self,
-                 fields=(dict(key='img', stack=True), dict(key='gt_bboxes'),
-                         dict(key='gt_labels'))):
-        self.fields = fields
-
-    def __call__(self, results):
-        """Call function to convert data in results to
-        :obj:`mmcv.DataContainer`.
-
-        Args:
-            results (dict): Result dict contains the data to convert.
-
-        Returns:
-            dict: The result dict contains the data converted to \
-                :obj:`mmcv.DataContainer`.
-        """
-
-        for field in self.fields:
-            field = field.copy()
-            key = field.pop('key')
-            results[key] = DC(results[key], **field)
-        return results
-
-    def __repr__(self):
-        return self.__class__.__name__ + f'(fields={self.fields})'
-
-
-@PIPELINES.register_module()
-class DefaultFormatBundle:
+class DefaultFormatBundleLP:
     """Default formatting bundle.
 
     It simplifies the pipeline of formatting common fields, including "img",
@@ -249,7 +178,7 @@ class DefaultFormatBundle:
 
 
 @PIPELINES.register_module()
-class Collect:
+class CollectLP:
     """Collect data from the loader relevant to the specific task.
 
     This is usually the last stage of the data loader pipeline. Typically keys
@@ -322,44 +251,3 @@ class Collect:
     def __repr__(self):
         return self.__class__.__name__ + \
             f'(keys={self.keys}, meta_keys={self.meta_keys})'
-
-
-@PIPELINES.register_module()
-class WrapFieldsToLists:
-    """Wrap fields of the data dictionary into lists for evaluation.
-
-    This class can be used as a last step of a test or validation
-    pipeline for single image evaluation or inference.
-
-    Example:
-        >>> test_pipeline = [
-        >>>    dict(type='LoadImageFromFile'),
-        >>>    dict(type='Normalize',
-                    mean=[123.675, 116.28, 103.53],
-                    std=[58.395, 57.12, 57.375],
-                    to_rgb=True),
-        >>>    dict(type='Pad', size_divisor=32),
-        >>>    dict(type='ImageToTensor', keys=['img']),
-        >>>    dict(type='Collect', keys=['img']),
-        >>>    dict(type='WrapFieldsToLists')
-        >>> ]
-    """
-
-    def __call__(self, results):
-        """Call function to wrap fields into lists.
-
-        Args:
-            results (dict): Result dict contains the data to wrap.
-
-        Returns:
-            dict: The result dict where value of ``self.keys`` are wrapped \
-                into list.
-        """
-
-        # Wrap dict fields into lists
-        for key, val in results.items():
-            results[key] = [val]
-        return results
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}()'
