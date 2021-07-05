7fbf1666db345b420d04bcdeba937ddc53bb60bb
anhdang000 <danglinh.anh000@gmail.com>
2021-07-05 15:34:21 +0700

    Update

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -12,7 +12,7 @@ from .utils import (NumClassCheckHook, get_loading_pipeline,
 from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
 from .xml_style import XMLDataset
-from .kitti import KittiDataset, KittiLPDataset
+from .kitti import KittiDataset, KittiDatasetLP
 
 __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'DeepFashionDataset',
@@ -21,5 +21,5 @@ __all__ = [
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
     'build_dataset', 'replace_ImageToTensor', 'get_loading_pipeline',
-    'NumClassCheckHook', 'KittiDataset', 'KittiLPDataset'
+    'NumClassCheckHook', 'KittiDataset', 'KittiDatasetLP'
 ]
