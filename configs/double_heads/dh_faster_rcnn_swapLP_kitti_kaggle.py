b33c81a08d650d284a9f5ea12bca59776d486d0b
anhdang000 <danglinh.anh000@gmail.com>
2021-07-22 13:15:43 +0700

    Change max epoch

--- configs/double_heads/dh_faster_rcnn_swapLP_kitti_kaggle.py
+++ configs/double_heads/dh_faster_rcnn_swapLP_kitti_kaggle.py
@@ -234,7 +234,7 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[8, 11])
-runner = dict(type='EpochBasedRunner', max_epochs=20)
+runner = dict(type='EpochBasedRunner', max_epochs=40)
 checkpoint_config = dict(interval=1)
 log_config = dict(interval=1, hooks=[dict(type='TextLoggerHook')])
 custom_hooks = [dict(type='NumClassCheckHook')]
