65146ec301090255ededfc8ae3b31707e9c6e389
anhdang000 <danglinh.anh000@gmail.com>
2021-09-19 13:04:40 +0700

    update batch_size

--- configs/double_heads/dh_faster_rcnn_model1_kitti_kaggle.py
+++ configs/double_heads/dh_faster_rcnn_model1_kitti_kaggle.py
@@ -147,8 +147,8 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=3,
-    workers_per_gpu=3,
+    samples_per_gpu=5,
+    workers_per_gpu=5,
     train=dict(
         type='KittiDatasetLP2',
         ann_file='train.txt',
