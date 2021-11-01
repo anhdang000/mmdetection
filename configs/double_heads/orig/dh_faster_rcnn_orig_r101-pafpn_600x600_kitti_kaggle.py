d2042af6cad3efefb0a12583f297eda06ad80943
anhdang000 <danglinh.anh000@gmail.com>
2021-11-01 23:39:02 +0700

    Update configs with PAFPN

--- configs/double_heads/orig/dh_faster_rcnn_orig_r101-pafpn_600x600_kitti_kaggle.py
+++ configs/double_heads/orig/dh_faster_rcnn_orig_r101-pafpn_600x600_kitti_kaggle.py
@@ -11,7 +11,7 @@ model = dict(
         style='pytorch',
         init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet101')),
     neck=dict(
-        type='FPN',
+        type='PAFPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
         num_outs=5),
